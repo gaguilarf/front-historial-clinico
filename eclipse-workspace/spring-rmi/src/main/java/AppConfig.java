0ec6d0ca8936f90308565fee8643ade0fba58d59
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-05-18 16:40:37 -0500

    feat(main): add AppConfig class

---
+++ eclipse-workspace/spring-rmi/src/main/java/AppConfig.java
@@ -0,0 +1,18 @@
+import java.rmi.RemoteException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.rmi.RmiServiceExporter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class AppConfig {
+	@Bean
+    public RmiServiceExporter rmiServiceExporter() throws RemoteException {
+        RmiServiceExporter exporter = new RmiServiceExporter();
+        exporter.setServiceName("CalculatorService");
+        exporter.setServiceInterface(CalculatorService.class);
+        exporter.setService(new CalculatorServiceImpl());
+        return exporter;
+    }
+}
