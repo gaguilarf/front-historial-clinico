bdef8fd9abd64ad74e65e12691987e83529d8bca
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-06-05 16:54:08 -0500

    feat(master): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SingInComponent } from './components/sing-in/sing-in.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'singIn', component: SingInComponent},
+    {path: 'error-page', component: ErrorPageComponent},
+    {path: '**', redirectTo: 'error-page', pathMatch: 'full'}
+];
+
