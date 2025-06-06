da1ed2cde04313e4e20e3e901bbdac8d58f0dffa
Gustavo Aguilar <gaguilarf@unsa.edu.pe>
2025-06-06 11:47:40 -0500

    feat(master): add login implementation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,45 @@
 import { Component } from '@angular/core';
 import { SpinnerComponent } from '../../shared/spinner/spinner.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [SpinnerComponent, RouterModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    SpinnerComponent
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  showPassword = false;
+  isLoading = false;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      dni: ['', [Validators.required, Validators.pattern('^[0-9]{8}$')]],
+      password: ['', Validators.required]
+    });
   }
 
-  logIn() {
-    console.log('Login button clicked');
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
   }
 
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      // Simulate API call
+      setTimeout(() => {
+        console.log('Login form submitted', this.loginForm.value);
+        this.isLoading = false;
+      }, 1500);
+    }
+  }
 }
