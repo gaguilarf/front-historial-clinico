f00d2955eea493db33d991ddaafdb96bb92c3e26
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-06-16 14:34:12 -0500

    feat(dashboard): desing menu

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../shared/material.module';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule, RouterModule, MaterialModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
