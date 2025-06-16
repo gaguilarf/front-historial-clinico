3f6ab5b14f39020b39a843aef875417781d44eea
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-06-16 14:35:23 -0500

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,21 @@ import { ErrorPageComponent } from './components/error-page/error-page.component
 import { ProfileComponent } from './components/dashboard/profile/profile.component';
 import { HomeComponent } from './layout/home/home.component';
 import { PatientsComponent } from './components/dashboard/patients/patients.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HelpComponent } from './components/dashboard/help/help.component';
+import { DepartmentsComponent } from './components/dashboard/departments/departments.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  //{ path: '', redirectTo: '/login', pathMatch: 'full' },
   {
-    path: 'dashboard',
-    component: HomeComponent,
+    path: '',
+    component: HomeComponent, // layout con el menú
     children: [
-      { path: '', redirectTo: 'patients', pathMatch: 'full' },
       { path: 'patients', component: PatientsComponent },
-      { path: 'profile', component: ProfileComponent }
+      { path: 'departments', component: DepartmentsComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'help', component: HelpComponent },
+      { path: '', component: DashboardComponent }, // Esto será "/"
     ]
   },
   { path: 'login', component: LoginComponent },
